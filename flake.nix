a01ea9b558815756156259e789a012be0ff95c3b
Bobby Rong <rjl931189261@126.com>
2022-03-21 12:43:18 +0800

    various cleanups

--- flake.nix
+++ flake.nix
@@ -3,37 +3,28 @@
   outputs = inputs@{ self, nixpkgs }: {
     nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [
-        ({ pkgs, config, ... }: {
-          imports = [ 
-            "${inputs.nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
-          ];
-          environment.enableDebugInfo = true;
-          services.xserver = {
+      modules = [{
+        imports = [ "${inputs.nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix" ];
+        environment.enableDebugInfo = true;
+        services.xserver = {
+          enable = true;
+          desktopManager.pantheon = {
             enable = true;
-            layout = "us";
-            desktopManager.pantheon = {
-              enable = true;
-              debug = true;
-            };
+            debug = true;
           };
-          users = {
-            mutableUsers = false;
-            extraUsers.test = {
-              isNormalUser = true;
-              uid = 1000;
-              extraGroups = [ "wheel" "networkmanager" ];
-              password = "test";
-            };
-          };
-          nixpkgs.config.allowAliases = false;
-          virtualisation = {
-            qemu.options = [ "-device intel-hda -device hda-duplex" ];
-            memorySize = 2048;
-            diskSize = 8192;
-          };
-        })
-      ];
+        };
+        users.users.test = {
+          isNormalUser = true;
+          uid = 1000;
+          extraGroups = [ "wheel" "networkmanager" ];
+          password = "test";
+        };
+        virtualisation = {
+          qemu.options = [ "-device intel-hda -device hda-duplex" ];
+          memorySize = 2048;
+          diskSize = 8192;
+        };
+      }];
     };
     packages.x86_64-linux.default = self.nixosConfigurations.vm.config.system.build.vm;
     apps.x86_64-linux.default = {
