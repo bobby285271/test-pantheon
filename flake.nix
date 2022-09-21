b922702a83c454c1f9c64309eea7bd702a2cb96d
Bobby Rong <rjl931189261@126.com>
2022-09-21 17:02:08 +0800

    flake: add some pkgs

--- flake.nix
+++ flake.nix
@@ -3,29 +3,43 @@
   outputs = inputs@{ self, nixpkgs }: {
     nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [{
-        imports = [ "${inputs.nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix" ];
-        environment.enableDebugInfo = true;
-        services.xserver = {
-          enable = true;
-          desktopManager.pantheon = {
-            enable = true;
-            debug = true;
+      modules = [
+        ({ pkgs, ... }: {
+          imports = [ "${inputs.nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix" ];
+          environment = {
+            enableDebugInfo = true;
+            systemPackages = with pkgs; [
+              ashpd-demo
+              d-spy
+              gdb
+              binutils
+              file
+              glib
+              htop
+              less
+            ];
           };
-        };
-        users.users.test = {
-          isNormalUser = true;
-          uid = 1000;
-          extraGroups = [ "wheel" "networkmanager" ];
-          password = "test";
-        };
-        virtualisation = {
-          qemu.options = [ "-device intel-hda -device hda-duplex" ];
-          memorySize = 2048;
-          diskSize = 8192;
-        };
-        system.stateVersion = "22.11";
-      }];
+          services.xserver = {
+            enable = true;
+            desktopManager.pantheon = {
+              enable = true;
+              debug = true;
+            };
+          };
+          users.users.test = {
+            isNormalUser = true;
+            uid = 1000;
+            extraGroups = [ "wheel" "networkmanager" ];
+            password = "test";
+          };
+          virtualisation = {
+            qemu.options = [ "-device intel-hda -device hda-duplex" ];
+            memorySize = 2048;
+            diskSize = 8192;
+          };
+          system.stateVersion = "22.11";
+        })
+      ];
     };
     packages.x86_64-linux.default = self.nixosConfigurations.vm.config.system.build.vm;
     apps.x86_64-linux.default = {
